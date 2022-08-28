8cdade9076232311b43955b1358f119df7399856
ashra325 <86569956+ashra325@users.noreply.github.com>
2022-08-27 18:32:33 -0700

    Add files via upload

---
+++ odd or even using if else.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+    int n=10;
+ int result=n%2;
+     if (result==0){
+      printf("the given number is even..");
+    }
+    else{
+      printf("\nthe given number is odd...");
+    }
+    printf("\n%d",0==2);//boolean expression
+    printf("\n%d",n%2==0);
+if (n%2==0){
+printf("\neven number....");
+}
+else{
+   printf("\nodd number...");
+}
+if(n%2==0)
+    printf("\neven number...");
+else
+    printf("\nodd number...");
+}
